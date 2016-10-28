4bfbbea5addb67fd5a475dc0924dcf2c5cab450f
akpersad <akpersad@gmail.com>
2016-10-28 16:41:29 -0400

    Reformatted spacing and indentation

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -13,25 +13,25 @@
         this.setActiveQuestion = setActiveQuestion;
 
         var numQuestionsAnswered = 0;
-        
+
         function setActiveQuestion() {
             var breakOut = false;
             var quizLengthAdjusted = DataService.quizQuestions.length - 1;
 
-            while (!breakOut){
+            while (!breakOut) {
                 this.activeQuestion = this.activeQuestion < quizLengthAdjusted ? ++this.activeQuestion : 0;
 
-                if(DataService.quizQuestions[this.activeQuestion].selected === null){
+                if (DataService.quizQuestions[this.activeQuestion].selected === null) {
                     breakOut = true;
                 }
             }
         }
-        
+
         function questionAnswered() {
             var quizLength = DataService.quizQuestions.length;
-            if(DataService.quizQuestions[this.activeQuestion].selected !== null){
+            if (DataService.quizQuestions[this.activeQuestion].selected !== null) {
                 numQuestionsAnswered++;
-                if(numQuestionsAnswered >= quizLength){
+                if (numQuestionsAnswered >= quizLength) {
                     //Placeholder
                 }
             }
