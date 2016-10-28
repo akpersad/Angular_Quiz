ee7804fcb3d81c2ce58f8d9f1a5eb6eb53b07c56
akpersad <akpersad@gmail.com>
2016-10-28 16:39:31 -0400

    Questions Incrementing

--- js/controllers/quiz.js
+++ js/controllers/quiz.js
@@ -10,9 +10,32 @@
         this.dataService = DataService;
         this.activeQuestion = 0;
         this.questionAnswered = questionAnswered;
+        this.setActiveQuestion = setActiveQuestion;
 
-        function questionAnswered() {
-            placeholder;
+        var numQuestionsAnswered = 0;
+        
+        function setActiveQuestion() {
+            var breakOut = false;
+            var quizLengthAdjusted = DataService.quizQuestions.length - 1;
+
+            while (!breakOut){
+                this.activeQuestion = this.activeQuestion < quizLengthAdjusted ? ++this.activeQuestion : 0;
+
+                if(DataService.quizQuestions[this.activeQuestion].selected === null){
+                    breakOut = true;
+                }
+            }
         }
-    };
+        
+        function questionAnswered() {
+            var quizLength = DataService.quizQuestions.length;
+            if(DataService.quizQuestions[this.activeQuestion].selected !== null){
+                numQuestionsAnswered++;
+                if(numQuestionsAnswered >= quizLength){
+                    //Placeholder
+                }
+            }
+            this.setActiveQuestion();
+        }
+    }
 })();
